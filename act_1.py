e1c222a6b8c089bdb60f1ff6fd1c1fa40cd05c5f
hardik1kalita1-debug <hardik1kalita1@gmail.com>
2026-02-21 20:28:49 +0530

    Add files via upload

---
+++ act_1.py
@@ -0,0 +1,21 @@
+name="Hardik"
+age=13
+is_student=True
+weight=45.11
+
+print("Name :", name)
+print("Datatype of Name is", type(name))
+print("age",age)
+print("Datatypr of age is: ",type(age))
+print("is_student:", is_student)
+print("Datatype of is_student :", type(is_student))
+print("Weight :",weight)
+print("Data Type of weight is", type(weight))
+
+print("\n After Type Casting.....")
+age=str(age)
+print(age)
+print("Datatype of age is:", type(age))
+weight=int(weight)
+print(weight)
+print("datatype of weight is:", type(weight) ) 
